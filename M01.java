38c295e67008cef7586894aa8580ea77dc95d711
Rahelsilaban <rahelj.e.silaban23@gmail.com>
2024-10-18 10:52:56 +0700

    Kami telah menyelesaikan Praktikum UTS

--- M01.java
+++ M01.java
@@ -1,2 +1,76 @@
-// NIM - Your Name
-// NIM - Your Name
+// 12S24010 - Artha Liebe Siregar
+// 12S24054 - Rahel Juri Elisabet Silaban
+
+
+import java.util.*;
+import java.lang.Math;
+
+public class M01 {
+    private static Scanner input = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        double performa, nilaiangka, a, aB, b, bC, c, d, e;
+        double kredit1;
+        String nilaihuruf, kredit;
+
+        a = 4.0;
+        aB = 3.5;
+        b = 3.0;
+        bC = 2.5;
+        c = 2.0;
+        d = 1.0;
+        e = 0.0;
+        kredit1 = 0;
+        kredit = input.nextLine();
+        if (kredit.equals("---")) {
+            kredit = kredit;
+        } else {
+            kredit1 = Integer.parseDouble(kredit);
+        }
+        nilaihuruf = input.nextLine();
+        while (!nilaihuruf.equals("---")) {
+            if (nilaihuruf.equals("A")) {
+                nilaiangka = 4.0;
+            } else {
+                if (nilaihuruf.equals("AB")) {
+                    nilaiangka = 3.5;
+                } else {
+                    if (nilaihuruf.equals("B")) {
+                        nilaiangka = 3.0;
+                    } else {
+                        if (nilaihuruf.equals("BC")) {
+                            nilaiangka = 2.5;
+                        } else {
+                            if (nilaihuruf.equals("C")) {
+                                nilaiangka = 2.0;
+                            } else {
+                                if (nilaihuruf.equals("D")) {
+                                    nilaiangka = 1.0;
+                                } else {
+                                    if (nilaihuruf.equals("E")) {
+                                        nilaiangka = 0.0;
+                                    } else {
+                                        nilaiangka = 0.0;
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            performa = kredit1 * nilaiangka;
+            System.out.println(toFixed(performa,1));
+            kredit = input.nextLine();
+            if (kredit.equals("---")) {
+                kredit = kredit;
+            } else {
+                kredit1 = Integer.parseDouble(kredit);
+            }
+            nilaihuruf = input.nextLine();
+        }
+    }
+    
+    private static String toFixed(double value, int digits) {
+        return String.format("%." + digits + "f", value);
+    }
+}
